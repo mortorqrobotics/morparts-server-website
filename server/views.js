431897bf1759b685e81dacebcd8231d72b409007
InsideOutside0 <sammy@salwen.org>
2017-10-27 16:08:46 -0700

    Some additions

--- server/views.js
+++ server/views.js
@@ -12,11 +12,11 @@ module.exports = function(imports) {
     }
 
     let renderPage = Promise.coroutine(function*(res, page, user, options) {
-        if (user) {
-            user.team = yield Team.findOne({
-                _id: user.team,
-            });
-        }
+        // if (user) {
+        //     user.team = yield Team.findOne({
+        //         _id: user.team,
+        //     });
+        // }
         res.render(webDir + "/src/page.html.ejs", {
             options: options || {},
             userInfo: user,
