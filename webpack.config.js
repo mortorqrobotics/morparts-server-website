fa9ee3783ad8f1d97369f93f003d8e41c6549093
InsideOutside0 <sammy@salwen.org>
2017-11-28 20:57:48 -0800

    Copypastes gulpfiles until webpack is fully implemented

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-"use strict";
-
-let path = require("path");
-let webpack = require("webpack");
-let htmlWebpackPlugin = require("html-webpack-plugin");
-let ExtractTextPlugin = require("extract-text-webpack-plugin");
-let StatsPlugin = require("stats-webpack-plugin");
-
-const basicPath = __dirname + "/website/src";
-
-module.exports = {
-  entry: {
-    home: basicPath + "/home/components/Home.js",
-
-  },
-  output: {
-    path: path.join(__dirname, "/dist/"),
-    filename: "[name]-[hash].min.js",
-    publicPath: "/[name]",
-  },
-  plugins: [
-   new HtmlWebpackPlugin({
-     template: "/website/page.html.ejs",
-     inject: 'body',
-     filename: 'index.html'
-   }),
-   new webpack.optimize.OccurrenceOrderPlugin(),
-   new webpack.HotModuleReplacementPlugin(),
-   new webpack.NoErrorsPlugin(),
-   new webpack.DefinePlugin({
-     'process.env.NODE_ENV': JSON.stringify('development')
-   }),
- ],
-}
+// "use strict";
+//
+// let path = require("path");
+// let webpack = require("webpack");
+// let htmlWebpackPlugin = require("html-webpack-plugin");
+// let ExtractTextPlugin = require("extract-text-webpack-plugin");
+// let StatsPlugin = require("stats-webpack-plugin");
+//
+// const basicPath = __dirname + "/website/src";
+// 
+// module.exports = {
+//   entry: {
+//     home: basicPath + "/home/components/Home.js",
+//
+//   },
+//   output: {
+//     path: path.join(__dirname, "/dist/"),
+//     filename: "[name]-[hash].min.js",
+//     publicPath: "/[name]",
+//   },
+//   plugins: [
+//    new HtmlWebpackPlugin({
+//      template: "/website/page.html.ejs",
+//      inject: 'body',
+//      filename: 'index.html'
+//    }),
+//    new webpack.optimize.OccurrenceOrderPlugin(),
+//    new webpack.HotModuleReplacementPlugin(),
+//    new webpack.NoErrorsPlugin(),
+//    new webpack.DefinePlugin({
+//      'process.env.NODE_ENV': JSON.stringify('development')
+//    }),
+//  ],
+// }
