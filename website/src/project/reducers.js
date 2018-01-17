7bed9892db33e02a6b45e573c4d882f50c6ea9a3
AriellaS <ariella.simoni@gmail.com>
2018-01-17 09:50:57 -0800

    Fix tree redux

--- website/src/project/reducers.js
+++ website/src/project/reducers.js
@@ -1,3 +1,5 @@
+import update from "react/lib/update";
+
 const project = ( state = {}, action) => {
     switch (action.type) {
         case "SET_PROJECT":
@@ -8,11 +10,32 @@ const project = ( state = {}, action) => {
 }
 
 const parts = ( state = [], action) => {
+    let newState = [];
+    let index;
     switch (action.type) {
         case "LOAD_PARTS":
             return state.concat(action.parts);
         case "ADD_PART":
-            return [action.part].concat(state);
+            index = state.findIndex(part => part._id === action.part.parent);
+            if (action.part.isAssembly) {
+                newState = update(state, {
+                    [index]: {
+                        childAssemblies: {
+                            $push: [action.part._id]
+                        }
+                    }
+                })
+            } else {
+                newState = update(state, {
+                    [index]: {
+                        childParts: {
+                            $push: [action.part._id]
+                        }
+                    }
+                })
+            }
+            newState = [action.part].concat(newState);
+            return newState;
         default:
             return state;
     }
