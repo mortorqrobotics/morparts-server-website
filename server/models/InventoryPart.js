07e7064274e99849f48fb1c1483b806b28d743a9
InsideOutside0 <sammy@salwen.org>
2018-08-18 22:19:06 -0700

    Adds Inventory and InventoryPart models

---
+++ server/models/InventoryPart.js
@@ -0,0 +1,42 @@
+"use strict"
+
+module.exports = function(imports) {
+
+    let mongoose = imports.modules.mongoose;
+    let Schema = mongoose.Schema;
+    let ObjectID = Schema.Types.ObjectID;
+
+    let inventoryPartSchema = new Schema({
+        name: {
+            type: String,
+            required: true,
+        },
+        quantity: {
+            type: Number,
+        },
+        inventory: {
+            type: ObjectID,
+            ref: "Inventory",
+            required: true,
+        },
+        purchaseLink: {
+            type: String,
+        },
+        created_at: Date,
+        updated_at: Date,
+    });
+
+    inventoryPartSchema.pre("save", function(next) {
+        let now = Date();
+        this.updated_at = now;
+        if (!this.created_at) {
+            this.created_at = now;
+        }
+        next();
+    });
+
+    let InventoryPart = mongoose.model("Inventory", inventoryPartSchema);
+
+    return InventoryPart;
+
+}
