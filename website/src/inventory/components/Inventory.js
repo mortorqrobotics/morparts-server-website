54731b2cb6ecec00d54f1ea11681c82ef3f7cb4a
InsideOutside0 <sammy@salwen.org>
2018-08-18 22:49:29 -0700

    Starts inventory page changes

--- website/src/inventory/components/Inventory.js
+++ website/src/inventory/components/Inventory.js
@@ -3,6 +3,7 @@ import Radium from "radium";
 
 import Root, { pageInit } from "~/shared/components/Root";
 import Navbar from "~/shared/components/navbar/Navbar";
+import FirstLoad from "~/inventory/components/FirstLoad";
 
 @Radium
 class Inventory extends React.Component {
@@ -10,7 +11,8 @@ class Inventory extends React.Component {
         return (
             <Root pageName="inventory">
                 <Navbar />
-                <h1>The inventory!</h1>
+                <FirstLoad />
+
             </Root>
         );
     }
